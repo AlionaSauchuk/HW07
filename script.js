4ef7fcf0077011a77f90aaa6aafc58657c3c4628
=AlionaSauchuk <=aliona.sauchuk@leverx.com>
2019-01-24 01:03:03 +0300

    hw 7

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 
 (function () {
-
     var data = JSON.stringify(false);
     var xhr = new XMLHttpRequest();
     
@@ -26,14 +25,68 @@
 })();
 
 
-function startUpdate(obj) {
-    sessionStorage.setItem('idUpdate', obj.id);
-    window.location.href = "http://127.0.0.1:5500/PUT/index.html";
+function POSTRequest(){
+    var formElements = document.getElementById("Form").elements;
+    var data = {};
+    for (var i = 0; i < formElements.length; i++) {
+        data[formElements[i].name] = formElements[i].value;
+    }
+
+    var xhr = new XMLHttpRequest();    
+    xhr.open("POST", "https://cors-anywhere.herokuapp.com/http://services.odata.org/V4/TripPinService/People");
+    xhr.setRequestHeader("Content-Type", "application/json");
+    xhr.addEventListener("readystatechange", function () {
+      if (this.readyState === 4) {
+        alert(data.UserName+' added');
+        var el = document.getElementById("data");
+        var json = JSON.parse(xhr.responseText);
+        Array.from(json.value).forEach(element => {
+            el.innerHTML += '<tr>' +
+                '<td><button class="btn btn-dark" id="' + element.UserName + '" onclick="DataUpdate(this)">Update</button></td>' +
+                '<td><button class="btn btn-dark" id="' + element.UserName + '" onclick="DataDelete(this)">Delete</button></td>' +
+                '<td>' + element.UserName + '</td>' +
+                '<td>' + element.FirstName + '</td>' +
+                '<td>' + element.LastName + '</td>' +
+                '<td>' + element.Gender + '</td></tr>';
+        });
+
+      }
+    });
+
+    xhr.send(data);
 }
 
-function startDelete(obj) {
+function DataUpdate(obj) {
+    var formElements = document.getElementById("Form").elements;
+    var data = {};
+    for (var i = 0; i < formElements.length; i++) {
+        data[formElements[i].name] = formElements[i].value;
+    }
 
-    sessionStorage.setItem('idDelete', obj.id);
-    window.location.href = "http://127.0.0.1:5500/DELETE/index.html";
+    var xhr = new XMLHttpRequest();  
+    var url = "https://services.odata.org/V4/(S(4bctqhjqdedzwim1pxenlomc))/TripPinServiceRW/People"  
+    xhr.open("PUT", url+'\'('+obj.id+'\')');
+    xhr.setRequestHeader("Content-Type", "application/json");
+    xhr.addEventListener("readystatechange", function () {
+      if (this.readyState === 4) {
+        alert(obj.id+' updated');
+      }
+    });
 
+    xhr.send(data);
 }
+
+function DataDelete(obj) {
+    var xhr = new XMLHttpRequest();  
+    var url = "https://services.odata.org/V4/(S(4bctqhjqdedzwim1pxenlomc))/TripPinServiceRW/People"  
+    xhr.open("DELETE", url+'\'('+obj.id+'\')');
+    xhr.setRequestHeader("Content-Type", "application/json");
+    xhr.addEventListener("readystatechange", function () {
+      if (this.readyState === 4) {
+        alert(obj.id+' deleted');
+      }
+    });
+
+    xhr.send(data);
+}
+
